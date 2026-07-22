021554257776d069f119c477f7e3323a2a3e0cac
nilsworld1635-sudo <nilsworld1635@gmail.com>
2026-07-22 20:41:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-console.log("Street Delights Loaded");
-function toggleMenu() {
-    const menu = document.getElementById("navLinks");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
 
-    if (menu.style.display === "flex") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "flex";
-        menu.style.flexDirection = "column";
-    }
-}
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
