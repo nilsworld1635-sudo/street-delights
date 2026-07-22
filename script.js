fee01c8978a59c557ffd6c9910bd6e4e18a7ee3a
nilsworld1635-sudo <nilsworld1635@gmail.com>
2026-07-22 18:08:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Street Delights Loaded");
+function toggleMenu() {
+    const menu = document.getElementById("navLinks");
+
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+        menu.style.flexDirection = "column";
+    }
+}
